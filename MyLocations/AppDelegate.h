5e8173d03f42204290ff7a73f88a605ae2828eb0
Ryan Robinson <ryan@ryanrobinson.name>
2014-05-04 18:08:56 -0400

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by Ryan Robinson on 5/4/14.
+//  Copyright (c) 2014 RRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by Ryan Robinson on 5/4/14.
+//  Copyright (c) 2014 RRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
