e66fc7e867a51d38e6b9024218ba9ac318182fec
Ryan Robinson <ryan@ryanrobinson.name>
2014-05-11 22:48:28 -0400

    added category picker screen

--- LocationDetailsViewController.m
+++ LocationDetailsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LocationDetailsViewController.h"
+#import "CategoryPickerViewController.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 
@@ -22,12 +23,14 @@
 @implementation LocationDetailsViewController
 {
     NSString *_descriptionText;
+    NSString *_categoryName;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     if ((self = [super initWithCoder:aDecoder])) {
         _descriptionText = @"";
+        _categoryName = @"No Category";
     }
     return self;
 }
@@ -37,7 +40,7 @@
     [super viewDidLoad];
     
     self.descriptionTextView.text = _descriptionText;
-    self.categoryLabel.text = @"";
+    self.categoryLabel.text = _categoryName;
     self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", self.coordinate.latitude];
     self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f",self.coordinate.longitude];
     
@@ -84,6 +87,23 @@
 - (void)closeScreen
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"PickCategory"]) {
+        CategoryPickerViewController *controller = segue.destinationViewController;
+        
+        controller.selectedCategoryName = _categoryName;
+    }
+}
+
+- (IBAction)categoryPickerDidPickCategory:(UIStoryboardSegue *)segue
+{
+    CategoryPickerViewController *viewController =  segue.sourceViewController;
+    
+    _categoryName = viewController.selectedCategoryName;
+    self.categoryLabel.text = _categoryName;
 }
 
 #pragma mark - UITableViewDelegate
---
+++ MyLocations/CategoryPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoryPickerViewController.h
+//  MyLocations
+//
+//  Created by Ryan Robinson on 5/11/14.
+//  Copyright (c) 2014 RRobinson. All rights reserved.
+//
+
+@interface CategoryPickerViewController : UITableViewController
+
+@property (nonatomic, strong) NSString *selectedCategoryName;
+
+@end
